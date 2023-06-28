a383315a382d677b1d4b8ce0d44498b7e0da6d91
AlexeyGrozdev <big.grozdev@gmail.com>
2023-06-28 11:45:55 +0300

    added working html template

--- blog/api/views.py
+++ blog/api/views.py
@@ -4,14 +4,16 @@ from blog.api.serializers import BlogSerializer
 from blog.models import Post
 
 from django.shortcuts import render
+from django.utils import timezone
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
 
 
 class BlogView(ModelViewSet):
 
-    queryset = Post.objects.all()
+    queryset = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     serializer_class = BlogSerializer
 
 
